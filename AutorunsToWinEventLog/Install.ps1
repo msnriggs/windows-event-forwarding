55702c9ee791f7f633509d1545feaa7d11725ca4
msnriggs <36765065+msnriggs@users.noreply.github.com>
2019-10-01 13:35:55 -0600

    Update Install.ps1

--- AutorunsToWinEventLog/Install.ps1
+++ AutorunsToWinEventLog/Install.ps1
@@ -1,11 +1,11 @@
 # Create Program Files directories
-$autorunsDir = "C:\Program Files\AutorunsToWinEventLog"
+$autorunsDir = "C:\Program Files\Perch"
 If(!(test-path $autorunsDir)) {
   New-Item -ItemType Directory -Force -Path $autorunsDir
 }
 
 # Download Autorunsc64.exe if it doesn't exist
-$autorunsPath = "c:\Program Files\AutorunsToWinEventLog\Autorunsc64.exe"
+$autorunsPath = "c:\Program Files\Perch\autoruns\Autorunsc64.exe"
 if(!(test-path $autorunsPath)) {
   # Requires TLS 1.2
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -15,7 +15,7 @@ if(!(test-path $autorunsPath)) {
 # Put a copy of the AutorunsToWinEventLog script in the Autoruns directory
 copy "$PSScriptRoot\AutorunsToWinEventLog.ps1" "$autorunsDir\AutorunsToWinEventLog.ps1"
 
-$ST_A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle hidden c:\PROGRA~1\AutorunsToWinEventLog\AutorunsToWinEventLog.ps1"
+$ST_A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle hidden c:\PROGRA~1\Perch\autoruns\AutorunsToWinEventLog.ps1"
 $ST_T = New-ScheduledTaskTrigger -Daily -At 11am
 $ST_P = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel Highest -LogonType ServiceAccount
 Register-ScheduledTask -TaskName "AutorunsToWinEventLog" -Action $ST_A -Trigger $ST_T -Principal $ST_P
